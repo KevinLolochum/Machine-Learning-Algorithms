3bfa0b16826b585ee6e0bf80d9fb11cabe539db7
KevinLolochum <49601901+KevinLolochum@users.noreply.github.com>
2020-11-21 20:34:55 -0500

    Add files via upload

---
+++ Linear Regression.py
@@ -0,0 +1,10 @@
+# Import libraries
+from sklearn.linear_model import LinearRegression
+# define data, create model and fit data
+X = Variables
+Y = f(x)
+Model = LinearRegression().fit(X, Y)
+# Score model
+Model.score(X, y)
+# Predict new values
+NewY = Model.Predict(NewX)
